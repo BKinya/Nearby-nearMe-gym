357cb63c4432a4ad79abc0abfb77f1b7d102e80d
Beatrice Kinya <gitongakinya1@gmail.com>
2018-06-15 12:01:55 -0700

    almost there

--- app/src/main/java/com/beatrice/nearby_nearme_gym/activities/set_profile_activity.java
+++ app/src/main/java/com/beatrice/nearby_nearme_gym/activities/set_profile_activity.java
@@ -2,13 +2,14 @@ package com.beatrice.nearby_nearme_gym.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 import com.beatrice.nearby_nearme_gym.R;
 
-public class set_profile_activity extends LifecycleLoggingActivity {
+public class set_profile_activity extends LifecycleLoggingActivity implements AdapterView.OnItemSelectedListener{
 
     private Spinner gender_spinner;
 
@@ -23,5 +24,16 @@ public class set_profile_activity extends LifecycleLoggingActivity {
                 R.array.gender_array, android.R.layout.simple_spinner_item);
         listAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         gender_spinner.setAdapter(listAdapter);
+        gender_spinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        parent.getItemAtPosition(position);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
