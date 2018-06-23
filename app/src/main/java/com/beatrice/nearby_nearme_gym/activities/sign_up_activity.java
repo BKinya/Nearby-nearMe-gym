8368e53dfab6f3e85cca1c3fd2d2cf54e27bd6db
BKinys <gitongakinya1@gmail.com>
2018-06-23 12:21:22 -0700

    sign up user validation

--- app/src/main/java/com/beatrice/nearby_nearme_gym/activities/sign_up_activity.java
+++ app/src/main/java/com/beatrice/nearby_nearme_gym/activities/sign_up_activity.java
@@ -2,6 +2,8 @@ package com.beatrice.nearby_nearme_gym.activities;
 
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -25,5 +27,56 @@ public class sign_up_activity extends LifecycleLoggingActivity {
 
         signup_btn_signup = findViewById(R.id.signup_btn_signup);
 
+        isUserInputValid();
+
     }
+
+    /**
+     *
+     * @return
+     */
+
+    public boolean isUserInputValid(){
+       name_edittxt.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+           @Override
+           public void onFocusChange(View view, boolean b) {
+               if (name_edittxt.getText().toString().length() == 0){
+                   name_edittxt.setError("Enter Your Name");
+                   return;
+               }
+           }
+       });
+
+       email_edittxt.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+           @Override
+           public void onFocusChange(View view, boolean b) {
+               if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email_edittxt.getText().toString()).matches()){
+                   email_edittxt.setError("Enter Valid Email Address");
+                   return;
+               }
+           }
+       });
+
+       password_edittxt.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+           @Override
+           public void onFocusChange(View view, boolean b) {
+               if (password_edittxt.getText().toString().length()<8){
+                   password_edittxt.setError("Too Short. Required length, at least 8 character");
+                   return;
+               }
+           }
+       });
+
+       confirm_password_edittxt.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+           @Override
+           public void onFocusChange(View view, boolean b) {
+               if (!confirm_password_edittxt.getText().toString().equals(password_edittxt.getText().toString())){
+                   confirm_password_edittxt.setError("Password does not match");
+                   return;
+               }
+           }
+       });
+        return true;
+    }
+
 }
