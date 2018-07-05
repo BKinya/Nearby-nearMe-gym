a6fe6d8891fca7b285151811b8195f8ce3191714
BKinys <gitongakinya1@gmail.com>
2018-07-05 03:40:16 -0700

    ------------

---
+++ app/src/main/java/com/beatrice/nearby_nearme_gym/model/LoginResponseObj.java
@@ -0,0 +1,14 @@
+package com.beatrice.nearby_nearme_gym.model;
+
+public class Login_usere {
+
+    private String mesage;
+
+    public String getMesage() {
+        return mesage;
+    }
+
+    public void setMesage(String mesage) {
+        this.mesage = mesage;
+    }
+}
